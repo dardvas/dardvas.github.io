74f22ebf80e3aa8888884b45b4c2f66b2f59b155
vvmul <dardvas@gmail.com>
2022-04-24 11:46:58 +0400

    added adjarian trip

---
+++ adjaria-23-04-22-makhuntseti-and-kapnitsavi/data.js
@@ -0,0 +1,609 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-makhuntseti-waterfall",
+      "name": "Makhuntseti Waterfall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.21275009871431827,
+        "pitch": -0.14318767048110104,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.666097923177672,
+          "pitch": 0.15030702253126016,
+          "rotation": 0,
+          "target": "1-makhuntseti-way-up"
+        },
+        {
+          "yaw": -3.005883666504978,
+          "pitch": 0.12241575121604598,
+          "rotation": 0,
+          "target": "2-makhuntseti-bridge-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-makhuntseti-way-up",
+      "name": "Makhuntseti way up",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.02156391930469148,
+        "pitch": -0.10784821036299874,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4196452392119348,
+          "pitch": 0.2592127059882916,
+          "rotation": 0,
+          "target": "0-makhuntseti-waterfall"
+        },
+        {
+          "yaw": 1.3705298192185396,
+          "pitch": 0.21456474321927566,
+          "rotation": 0,
+          "target": "2-makhuntseti-bridge-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-makhuntseti-bridge-1",
+      "name": "Makhuntseti bridge 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1728079248254666,
+        "pitch": 0.0014417195689517825,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.548402597107227,
+          "pitch": -0.2624978375001241,
+          "rotation": 0,
+          "target": "0-makhuntseti-waterfall"
+        },
+        {
+          "yaw": 1.2327562455063426,
+          "pitch": -0.12969046491143388,
+          "rotation": 0,
+          "target": "4-gate-of-makhuntseti"
+        },
+        {
+          "yaw": -1.2009749657691646,
+          "pitch": -0.058062524225846346,
+          "rotation": 0,
+          "target": "3-makhuntseti-bridge-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-makhuntseti-bridge-2",
+      "name": "Makhuntseti bridge 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07211094292379805,
+        "pitch": 0.17035723211836995,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06001865681622576,
+          "pitch": 0.3529064481103177,
+          "rotation": 0,
+          "target": "2-makhuntseti-bridge-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-gate-of-makhuntseti",
+      "name": "Gate of Makhuntseti",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.5785615791022742,
+        "pitch": 0.0456551986841216,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.431295647431579,
+          "pitch": -0.02664447208174714,
+          "rotation": 0,
+          "target": "2-makhuntseti-bridge-1"
+        },
+        {
+          "yaw": 2.5881297533878316,
+          "pitch": -0.2464591437266499,
+          "rotation": 0,
+          "target": "5-next-to-the-gate-of-makhuntseti"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-next-to-the-gate-of-makhuntseti",
+      "name": "Next to the Gate of Makhuntseti",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.29162463451044296,
+        "pitch": 0.1348004340858342,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8519047103556172,
+          "pitch": 0.2649022168031401,
+          "rotation": 0,
+          "target": "4-gate-of-makhuntseti"
+        },
+        {
+          "yaw": 1.1214242980731832,
+          "pitch": 0.007782266822184525,
+          "rotation": 0,
+          "target": "6-kapnistavi-village"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kapnistavi-village",
+      "name": "Kapnistavi village",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08258933001501667,
+        "pitch": 0.23108645843287867,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9667085156897537,
+          "pitch": -0.09308243032582197,
+          "rotation": 0,
+          "target": "5-next-to-the-gate-of-makhuntseti"
+        },
+        {
+          "yaw": 0.03803658434908819,
+          "pitch": 0.18762979050477036,
+          "rotation": 0,
+          "target": "7-kapnistavi-waterfall-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-kapnistavi-waterfall-1",
+      "name": "Kapnistavi Waterfall 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.062123593768055,
+        "pitch": 0.2806367423947158,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5242198456642235,
+          "pitch": 0.2544052451762546,
+          "rotation": 0,
+          "target": "8-kapnistavi-waterfall-2"
+        },
+        {
+          "yaw": -2.7753178548999102,
+          "pitch": -0.038261002395239885,
+          "rotation": 0,
+          "target": "6-kapnistavi-village"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-kapnistavi-waterfall-2",
+      "name": "Kapnistavi Waterfall 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.135375201784857,
+        "pitch": 0.14423502926033827,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3751628898715005,
+          "pitch": 0.13045031360421433,
+          "rotation": 0,
+          "target": "7-kapnistavi-waterfall-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-kapnistavi-waterfall---watermill",
+      "name": "Kapnistavi Waterfall - watermill",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07637197005013974,
+        "pitch": 0.4031875927757351,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0648574364533907,
+          "pitch": 0.3858405454626048,
+          "rotation": 0,
+          "target": "8-kapnistavi-waterfall-2"
+        },
+        {
+          "yaw": 3.031069981658587,
+          "pitch": 0.10473824682364707,
+          "rotation": 0,
+          "target": "10-kapnistavi-waterfall-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-kapnistavi-waterfall-3",
+      "name": "Kapnistavi Waterfall 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.7883264999948763,
+        "pitch": 0.2964215450528549,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6694684537195217,
+          "pitch": 0.1308428253036027,
+          "rotation": 0,
+          "target": "8-kapnistavi-waterfall-2"
+        },
+        {
+          "yaw": -3.048803946951745,
+          "pitch": 0.09958484984708704,
+          "rotation": 0,
+          "target": "12-kapnistavi-tree-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-kapnistavi-village-2",
+      "name": "Kapnistavi village 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.2430543620134369,
+        "pitch": 0.09161303237214113,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.040921419755310495,
+          "pitch": 0.02148561416801087,
+          "rotation": 0,
+          "target": "10-kapnistavi-waterfall-3"
+        },
+        {
+          "yaw": -2.52878724923573,
+          "pitch": -0.06486322957704616,
+          "rotation": 0,
+          "target": "12-kapnistavi-tree-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-kapnistavi-tree-house",
+      "name": "Kapnistavi Tree house",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0995618061454078,
+        "pitch": 0.162905901458835,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7820896183511614,
+          "pitch": 0.16123321165632376,
+          "rotation": 0,
+          "target": "11-kapnistavi-village-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-kapnistavi-village-3",
+      "name": "Kapnistavi village 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.854616475807859,
+        "pitch": 0.26393886360414953,
+        "fov": 1.3865187505972731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.342822212314614,
+          "pitch": 0.16231905680172787,
+          "rotation": 0,
+          "target": "11-kapnistavi-village-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Adjaria 23.04.22 - Makhuntseti & Kapnistavi",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
